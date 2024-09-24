299dd40102396bfb943ecde7bd5ee080a10835e8
migcarde <mcaraballodelarosa@outlook.com>
2024-09-24 20:25:00 +0200

    Added main theme

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData mainTheme() => ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(
+            0xFF9dd4ed,
+          ),
+        ),
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/core/app_theme.dart';
 import 'package:weather/core/location_service.dart';
 import 'package:weather/ui/location/cubit/location_cubit.dart';
 import 'package:weather/ui/location/location_widget.dart';
@@ -14,6 +15,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: AppTheme.mainTheme(),
       home: Scaffold(
         body: Center(
           child: BlocProvider(
