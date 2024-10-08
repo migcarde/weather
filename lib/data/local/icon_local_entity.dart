943c30b042ba3c49a01f99c555214af63387f694
migcarde <mcaraballodelarosa@outlook.com>
2024-10-08 23:16:06 +0200

    feat: added icon call to api

---
+++ lib/data/local/icon_local_entity.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+import 'package:weather/data/local/local_types_ids.dart';
+
+@HiveType(typeId: LocalTypesIds.iconId)
+class IconLocalEntity extends HiveObject with EquatableMixin {
+  @HiveField(0)
+  final int id;
+
+  @HiveField(1)
+  final List<int> image;
+
+  IconLocalEntity({
+    required this.id,
+    required this.image,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        image,
+      ];
+}
--- lib/data/local/local_types_ids.dart
+++ lib/data/local/local_types_ids.dart
@@ -1,3 +1,5 @@
 class LocalTypesIds {
   static const int weatherTypeId = 1;
+  static const int iconId = 2;
+  static const int timeHourlyId = 3;
 }
